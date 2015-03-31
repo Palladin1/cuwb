8f35a4b31a32876c4c51bdf2ec06a75b7f6a9ad4
Palladin <participant@email.ua>
2015-03-31 19:13:38 +0300

    add code

--- registrator.c
+++ registrator.c
@@ -2,11 +2,12 @@
 #include  <stdlib.h>
 
 
-#define  R_PWD_LEN            6+1 
-#define  R_ERR_CODE_LEN       4+1
-#define  R_CRC_LEN            4+1
-#define  R_SEND_DATA_LEN    120+1
-#define  R_RECIV_DATA_LEN    20+1
+#define  R_PWD_LEN            6 
+#define  R_ERR_CODE_LEN       4
+#define  R_CRC_LEN            4
+#define  R_STATUS_LEN         6
+#define  R_SEND_DATA_LEN     20
+#define  R_RECIV_DATA_LEN    20
  
 
 struct registrator_status {
@@ -19,29 +20,24 @@ struct registrator_status {
 };
 
 struct registrator_send_message {
-    u08 soh;
     u08 len;
     u08 seq;
     u08 cmd;
     u08 pwd[R_PWD_LEN];
     u08 data[R_SEND_DATA_LEN];
-    u08 enq;
+    u08 data_len;
     u08 bcc[R_CRC_LEN];
-    u08 etx;
 };
     
 struct registrator_recive_message {
-    u08 soh;
     u08 len;
     u08 seq;
     u08 cmd;
     u08 error_code[R_ERR_CODE_LEN];
     u08 data[R_RECIV_DATA_LEN];
-    u08 eot;
-    struct registrator_status status;
-    u08 enq;
+    u08 data_len;
+    u08 status[R_STATUS_LEN];
     u08 bcc[R_CRC_LEN];
-    u08 etx;
 };
 
 static struct registrator_send_message send_message; 
@@ -53,15 +49,80 @@ static u32 timer_var;
 
 void RegistratorInit (void)
 {
-    int i;
-    
+    u08 i;
     registrator_status = WAIT;
     timer_var = 0;
     
-    send_message.soh = RDATA_SOH;
-    send_message.enq = RDATA_ENQ;
-    send_message.etx = RDATA_ETX;
+    send_message.cmd = 0;
+    send_message.seq = 0;
+    send_message.data[0] = '\0';
+    
+    /* set standart pwd */ 
+    for (i = 0; i < R_PWD_LEN; i++) {
+       send_message.pwd[i] = '0';
+    }
 } 
+
+
+void RegistratorProcessing (u08 time_correcting)
+{
+    u08 ans;
+    static enum processing_state { 
+        P_IDDLE,
+        P_REQUEST,
+        P_CANSELL,
+        P_ANSWER
+    } state = IDDLE;
+   
+    timer_var++;
+    
+    switch (state) {
+        case P_IDDLE: {
+             if (time_var == R_TIMEOUT_WAIT_CHACK) {
+                 send_message.cmd = ALLOW;
+                 timer_var = 0;
+                 registrator_status = WAIT;
+                 state = P_REQUEST;
+             }
+             break;
+        }
+        case P_REQUEST: {
+             RegistratorFrameSend();
+             state = P_ANSWER;
+             break;
+        }
+        case P_ANSWER: {
+             cmd = RegistratorFrameGet(xxx);
+             if (cmd == RANSVER_SYN) {
+                 timer_var = 0;
+             }
+             else if (cmd = RANSVER_NAK) {
+                 timer_var = 0;
+                 send_message.seq = increase_seq(send_message.seq);
+                 state = P_REQUEST;                     
+             }
+             else if (cmd = RANSVER_AK) {
+                 timer_var = 0;
+                 send_message.seq = increase_seq(send_message.seq);
+                 state = P_IDDLE;                     
+             }
+             state = P_ANSWER;
+             break;
+        }
+        case P_REQUEST: {
+             RegistratorFrameSend();
+             state = P_ANSWER;
+             break;
+        }
+        
+             
+            
+u08 increase_seq (u08 cur)
+{
+    return (cur++ < SEQ_VALUE_MAX) ? cur : 0;  
+}     
+    
+
 
 void RegistratorDataSet (u08 cmd, u32 data[], u08 ndata) 
 {
@@ -76,7 +137,7 @@ void RegistratorDataSet (u08 cmd, u32 data[], u08 ndata)
         offset++;
     }
     
-    send_message.data[offset] = '\0';
+    send_message.data_len = offset;
 }
 
 REGISTRATOR_STATUS RegistratorStatusGet (void) 
@@ -85,7 +146,187 @@ REGISTRATOR_STATUS RegistratorStatusGet (void)
 }
     
 
-void crc (u08 * crc, u32 d) 
+typedef enum frame_state {
+    FRAME_WAIT,
+    FRAME_LEN,
+    FRAME_SEQ,
+    FRAME_CMD,
+    FRAME_ERROR_CODE,
+    FRAME_DATA, 
+    FRAME_STATUS,
+    FRAME_CRC,    
+    FRAME_END 
+};
+
+u08 RegistratorFrameGet (u08 c) 
 {
-    u32 crc_cnt = 0;
+    static frame_state cur_state;
+    static u32 crc_cnt;
+    static u08 i;
+    u08 ret = 0;
+    
+    switch (c) {
+        case RDATA_SOH: {
+             cur_state = FRAME_LEN;
+             break;
+        }
+        case RANSVER_NAK: {
+             ret = RANSVER_NAK;
+             break;
+        }
+        case RANSVER_SYN: {
+             ret = RANSVER_SYN;
+             break;
+        }
+    }
+
+    switch (cur_state) {
+        case FRAME_WAIT: {
+             break;
+        }
+        case FRAME_LEN: {
+             i = 0;
+             crc_cnt = c;
+             receive_message.len = (0x20 <= c) ? (c - 0x20) : 0;            
+             cur_state = (receive_message.len == 0) ? FRAME_WAIT : FRAME_SEQ;
+             break;
+        }
+        case FRAME_SEQ: {
+             crc_cnt += c;
+             receive_message.seq = (0x20 <= c && c <= 0x7F) ? (c - 0x20) : 0; 
+             cur_state = (receive_message.seq == 0) ? FRAME_WAIT : FRAME_CMD;
+             break;
+        }
+        case FRAME_CMD: {
+             crc_cnt += c;
+             receive_message.cmd = (0x20 <= c) ? c : 0;
+             cur_state = ()
+             break;
+        }
+        case FRAME_ERROR_CODE: {
+             crc_cnt += c;
+             if (c == RDATA_SEPARATOR) {
+                 i = 0;
+                 cur_state = FRAME_DATA;
+             }
+             else if (i < R_ERR_CODE_LEN) {
+                 receive_message.error_code[i] = c;
+                 i++;
+             }
+             else {
+                 cur_state = FRAME_WAIT;
+             }
+             break;
+        }
+        case FRAME_DATA: {
+             crc_cnt += c;
+             if (c == RDATA_EOT) {
+                 receive_message.data_len = i;
+                 i = 0;
+                 cur_state = FRAME_STATUS;
+             }
+             else if (i < R_DATA_LEN) {
+                 receive_message.status = c;
+                 i++;
+             }
+             else {
+                 receive_message.data_len = 0;
+                 cur_state = FRAME_WAIT;
+             }
+             break;
+        }
+        case FRAME_STATUS: {
+             crc_cnt += c;
+             if (c == RDATA_ENQ) {
+                 i = 0;
+                 cur_state = FRAME_CRC;
+             }
+             else if (i < R_DATA_LEN) {
+                 receive_message.status[i] = c;
+                 i++;
+             }
+             else {
+                 cur_state = FRAME_WAIT;
+             }
+             break;
+        }  
+        case FRAME_CRC: {
+             if (c == RDATA_ETX) {
+                 i = 0;
+                 
+                 if (atoin(receive_message.crc) == (u16)(crc_cnt & 0x0000FFFF)) {
+                     ret = RANSVER_NAK;;
+                 }
+                 cur_state = FRAME_WAIT;
+             }
+             else if (i < R_CRC_LEN) {
+                 receive_message.crc[i] = c;
+                 i++;
+             }
+             else {
+                 cur_state = FRAME_WAIT;
+             }
+             break;
+        } 
+//        default : {
+//             break;
+//        }
+    }
+         
+    return ret;
+}  
+
+void RegistratorFrameSend (void)
+{
+    u08 c;
+    
+    send_message.len = 1 + 1 + 1 + R_PWD_LEN + send_message.data_len + 1;
+    
+    c = RDATA_SOH;
+    sendnstr(c, 1);    
+    sendnstr(send_message.len + 0x20, 1);
+    sendnstr(send_message.seq + 0x20, 1);
+    sendnstr(send_message.cmd, 1);    
+    sendnstr(send_message.pwd, R_PWD_LEN);
+    c = RDATA_SEPARATOR;
+    sendnstr(c, 1);
+    sendnstr(send_message.data, send_message.data_len);
+    c = RDATA_ENQ;
+    sendnstr(c, 1);
+    sendnstr(send_message.data, send_message.data_len);
+    
+    makecrc(send_message.crc);
+    sendnstr(send_message.crc, R_CRC_LEN);
+    
+    c = RDATA_ENQ;
+    sendnstr(c, 1);
+}
+        
+    
+void makecrc (u08 * crc) 
+{
+    u08 i;
+    u32 crc = 0;
+    crc = RDATA_SOH + RDATA_SEPARATOR + RDATA_ENQ + RDATA_ETX;
+    
+    for (i = 0; i < R_PWD_LEN; i++)
+        crc += send_message.pwd[i];
+        
+    for (i = 0; i < send_message.data_len; i++)
+        crc += send_message.data[i];
+    
+    crc &= 0x0000FFFF;
+
+    i = R_CRC_LEN;
+    while (i-- > 0) {
+        send_message.crc[i] = crc % 10 + '0';
+        crc /= 10;
+    }
+}
+
+extern void (* putbyte)(u08);
+void sendnstr (u08 *s, u08 len)
+{
+    while (len-- > 0)
+        (* putbyte)(*s++);
 }
--- registrator.h
+++ registrator.h
@@ -1,7 +1,8 @@
 #ifndef  REGISTRATOR_H
 #define  REGISTRATOR_H
 
-#define  REGISTRATOR_TIMEOUT    200
+#define  R_TIMEOUT_WAIT_ACK      200
+#define  R_TIMEOUT_WAIT_CHACK    1000
 
 #define  RCMD_ALLOW    7Ch
 #define  RCMD_SELL     7Bh
@@ -9,12 +10,17 @@
 
 #define  RANSVER_NAK    15h
 #define  RANSVER_SYN    16h
+#define  RANSVER_AK     14h
 
 #define  RDATA_SOH    01h
 #define  RDATA_EOT    04h
 #define  RDATA_ENQ    05h
 #define  RDATA_ETX    03h
 #define  RDATA_SEPARATOR    ';'
+
+#define  SEQ_VALUE_LOWER   0x20
+#define  SEQ_VALUE_UPPER   0x7F
+#define  SEQ_VALUE_MAX    (SEQ_VALUE_UPPER - SEQ_VALUE_LOWER)
 
 #ifndef u08
 typedef unsigned char u08;
